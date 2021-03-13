4d01209a7d7a6f2d6400edca46546dde102833de
aCupOfHotJava <71524931+aCupOfHotJava@users.noreply.github.com>
2021-03-12 19:33:22 -0800

    Brandon initial workspace

---
+++ Brandon/team-00/src/main/java/ubc/cosc322/Amazon.java
@@ -0,0 +1,24 @@
+package ubc.cosc322;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
+import sfs2x.client.entities.Room;
+import ygraph.ai.smartfox.games.BaseGameGUI;
+import ygraph.ai.smartfox.games.GameClient;
+import ygraph.ai.smartfox.games.GameMessage;
+import ygraph.ai.smartfox.games.GamePlayer;
+import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
+
+public class Amazon {
+
+	public GameMessage playerMove() {
+		
+		GameMessage moveMsg = new GameMessage(GameMessage.GAME_ACTION_MOVE);
+		
+		return moveMsg;
+		
+	}
+	
+}
