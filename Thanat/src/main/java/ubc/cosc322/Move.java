cfb7201718e8f63291d6ef5201914a8e2acd27cd
thanat1999 <tnt.naranong@gmail.com>
2021-03-15 20:28:31 +0700

    Move Class

--- Thanat/src/main/java/ubc/cosc322/Move.java
+++ Thanat/src/main/java/ubc/cosc322/Move.java
@@ -1,5 +1,48 @@
 package ubc.cosc322;
 
-public class Move {
+import java.util.*;
 
+import ygraph.ai.smartfox.games.GameClient;
+
+public class Move {
+	
+	private ArrayList<Integer> queenPosCurrent;
+	private ArrayList<Integer> queenPosNew;
+	private ArrayList<Integer> arrowPos;
+	
+	
+	public ArrayList<Integer> getQueenPosCurrent() {
+		return queenPosCurrent;
+	}
+
+	public ArrayList<Integer> getQueenPosNew() {
+		return queenPosNew;
+	}
+
+	public ArrayList<Integer> getArrowPos() {
+		return arrowPos;
+	}
+
+	public void setQueenPosCurrent(ArrayList<Integer> queenPosCurrent) {
+		this.queenPosCurrent = queenPosCurrent;
+	}
+
+	public void setQueenPosNew(ArrayList<Integer> queenPosNew) {
+		this.queenPosNew = queenPosNew;
+	}
+
+	public void setArrowPos(ArrayList<Integer> arrowPos) {
+		this.arrowPos = arrowPos;
+	}
+
+	public Move(ArrayList<Integer> queenPosCurrent, ArrayList<Integer> queenPosNew, ArrayList<Integer> arrowPos) {
+		this.queenPosCurrent = queenPosCurrent;
+		this.queenPosNew = queenPosNew;
+		this.arrowPos = arrowPos;
+	
+		//GameClient.sendMoveMessage(this.queenPosCurrent, this.queenPosNew, this.arrowPos);
+	}
+	
+	
+	
 }
